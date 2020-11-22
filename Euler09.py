61fdd99a8b4ad4e10a2b1d1fe75facc673e12c95
Jaka <jaka.cop@gmail.com>
2020-11-22 12:47:15 +0100

    Deveta naloga.

--- Euler09.py
+++ Euler09.py
@@ -2,7 +2,7 @@ import math
 def pitagora(n):
     for a in range(2, n):
         for b in range(2, n): 
-            for c in range(2, n):
+            for c in range(2, n // 2):
                 if a ** 2 + b ** 2 == c ** 2 and a + b + c == n:
                     return a, b, c
 
