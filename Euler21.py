ed7e772d180ad5a3db740d2047a3f1a2bbc45441
Jaka <jaka.cop@gmail.com>
2021-01-10 14:54:28 +0100

    Enaindvajseta naloga.

--- Euler21.py
+++ Euler21.py
@@ -1,17 +1,19 @@
-def delitelji(n):
-    sez = []
+import math
+def vsota_deliteljev(n):
+    delitelji = []
     for i in range(1, n):
         if n % i == 0:
-            sez.append(i)
-    return sez
+            delitelji.append(i)
+    return sum(set(delitelji))
 
-def sorodni_st(m, n):
-    for i in range(1, max(m, n) + 1):
-        if sum(delitelji(m)) == n and sum(delitelji(n)) == m:
-            return True
-        return False
+#Ce je stevilo popolni kvadrat npr. 9 potem bo drugace 3 stelo dvakrat!!! <------Ce pristevamo pare, kar je sicer veliko hitreje.
 
 def resitev(n):
     vsota = 0
-    for i in range(1, n + 1):
-        if
+    for i in range(1, n):
+        a = vsota_deliteljev(vsota_deliteljev(i))
+        if i == a and i != vsota_deliteljev(i):
+            vsota += i
+    return vsota
+
+print(resitev(10000))
