850a50463022b8b4da4f31458ea9d31195d83523
Jaka <jaka.cop@gmail.com>
2021-02-21 20:04:04 +0100

    Enainstirideseta naloga.

--- Euler41.py
+++ Euler41.py
@@ -1,28 +1,25 @@
-from itertools import permutations 
 def je_prastevilo(n):
     if n == 1:
         return False
     else:
-        for i in range(2, int(n ** 0.5) + 1):
+        for i in range(2, int(n ** 0.5 + 1)):
             if n % i == 0:
                 return False
     return True
 
-#def vsota_stevk(n):
-#    vsota = 0
-#    while n:
-#        vsota += (n % 10)
-#        n //= 10
-#    return vsota
+def je_pandigitalno(n):
+    sez = []
+    kontrola = [1, 2, 3, 4, 5, 6, 7]
+    while n:
+        sez.append(n % 10)
+        n //= 10
+    if len(set(sez)) == len(sez) and all(elem in sez for elem in kontrola):
+        return True
+    return False
 
-#def prastevilo(n):
-#    for i in range(n):
-#        najvecje = 0
-#        if (vsota_stevk(i) == 45) and (je_prastevilo(i)) and (i > najvecje):
-#            najvecje = i
-#    return najvecje
+def resitev(n):
+    for i in range(n, -1, -1):
+        if je_prastevilo(i) and je_pandigitalno(i):
+            return i
 
-def permutacije(m):
-    return list(permutations(range(1, m + 1)))
-
-print(permutacije(3))
+print(resitev(7777777))
